d32f3cbcd23e69225fee3aad0f2c71148a380c3a
Arulmozhi936 <46783024+Arulmozhi936@users.noreply.github.com>
2019-07-17 21:16:12 +0530

    Update pro4file.py

--- pro4file.py
+++ pro4file.py
@@ -1,11 +1,11 @@
 m12,m22=input().split()
-a=0
+ma=0
 if len(m12)>len(m22):
-   m12,m22=m22,m12
-   q=0
-   while q<len(m12):
-   a+=(ord(m22[q])-ord(m12[q]))
-   q+=1
-   for q in range(q,len(m22)):
-   a+=ord(m22[q])-ord('a')+1
-   print(a)
+    m12,m22=m22,m12
+q=0
+while q<len(m12):
+    ma+=(ord(m22[q])-ord(m12[q]))
+    q+=1
+for q in range(q,len(m22)):
+    ma+=ord(m22[q])-ord('a')+1
+print(ma)
