819c4ea9fe6293af6b9078bec7868e038c33ad70
Arulmozhi936 <46783024+Arulmozhi936@users.noreply.github.com>
2019-07-17 13:52:32 +0530

    Update pro3file.py

--- pro3file.py
+++ pro3file.py
@@ -1,10 +1,10 @@
 a,s=raw_input().split()
-o=abs(len(a)-len(s))
+o=abs(len(a) - len(s))
 for i in range(len(a)):
-if len(s)==1 and n[i] in a:
-break
-if i>=len(a)  or i>=len(s):
-break
-if n[i]!=n[i] and n[i]:
-o=o+1
+     if len(s)==1 and s[i] in a:
+            break
+     if i>=len(a)  or i>=len(s):
+            break
+     if s[i]!=s[i] and s[i]:
+            o=o+1
 print(o)
