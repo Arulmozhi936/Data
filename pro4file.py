f99364daf0fff55acf2026a2cb8a9194b5dd79c4
Arulmozhi936 <46783024+Arulmozhi936@users.noreply.github.com>
2019-07-17 14:03:10 +0530

    Update pro4file.py

--- pro4file.py
+++ pro4file.py
@@ -4,7 +4,7 @@ if len(m12)>len(m22):
    m12,m22=m22,m12
    q=0
    while q<len(m12):
-   a+=(ord(m22[q])-ord(m12[q])
+   a+=(ord(m22[q])-ord(m12[q]))
    q+=1
    for q in range(q,len(m22)):
    a+=ord(m22[q])-ord('a')+1
